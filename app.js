bf5a46e44dbaf4f10a6e7a397f04bba5d8edad31
sina <sinabhbh8383@gmail.com>
2022-05-19 12:56:05 +0430

    min aded

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const body_parser = require('body-parser');
+const path = require('path');
 
 const app =  express();
-
 
 //costom midelwer
 app.use(body_parser.urlencoded({extended:false}))
 
+app.set("view engine" , "ejs")
+app.set("views" , "views")
 
+app.use(express.static(path.join(__dirname , "public")))
+
+app.get("/" ,(req , res )=>{
+    res.render("index" ,{
+        title:"کارهای روزمره"
+    })
+})
 
 const port = 4000
 app.listen( port,
